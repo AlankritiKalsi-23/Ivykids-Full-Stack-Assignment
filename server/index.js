17a9bc606bfd07eb5cc77324f586e7bafa3de12a
Alankriti <ak5142@srmist.edu.in>
2023-09-16 00:07:00 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -10,17 +10,13 @@ import tweetRoutes from "./routes/tweets.js";
 const app = express();
 dotenv.config();
 
-const connect = () => {
-  mongoose.set("strictQuery", false);
-  mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-      console.log("connect to mongodb database");
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
+const uri = process.env.ATLAS_URI;
+//useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
+mongoose.connect(uri, {});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.use(cookieParser());
 app.use(express.json());
@@ -28,7 +24,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
 
-app.listen(8000, () => {
-  connect();
-  console.log("Listening to port 8000");
+app.listen(3000, () => {
+  //connect();
+  console.log("Listening to port 3000");
 });
